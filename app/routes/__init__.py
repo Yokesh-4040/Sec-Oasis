a93f923e9830652cd40cdb3f81be54346d41b5a9
Yokesh <Yokesh@fourtyfourty.in>
2025-07-01 23:25:12 +0530

    Dump

--- app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth import auth_bp
-from .employee import employee_bp
-from .attendance import attendance_bp
-from .views import views_bp
-
-__all__ = ['auth_bp', 'employee_bp', 'attendance_bp', 'views_bp'] 
